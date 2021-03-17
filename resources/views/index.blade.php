6c6c92496b1f139c08e41390b85c5cac37390f89
Josue M <joshx210@gmail.com>
2021-03-17 17:33:44 -0600

    Agregando TailwindCss a Vista

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,22 +4,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>API Explained</title>
+    <!-- Tailwind -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
   </head>
-  <body>
-      <div class="container-fluid">
-          <table class="table table-sm">
-              <thead>
-                  <th>Posts</th>
-              </thead>
-              <tbody>
+  <body class="bg-gray-100 text-gray-700">
+      <div class="container-fluid mx-auto px-4">
+          <div class="grid grid-cols-3 my-10">
                 @foreach ($posts as $post)
-                <div>
-                  <h4>{{ $post->title }}</h4>
+                <div class="bg-white hover:bg-blue-100 border border-gray-200 p-5">
+                    <h4 class="font-bold text-lg mb-4">{{ $post->title }}</h4>
+                    <p class="text-xs">{{ $post->excerpt }}</p>
+                    <p class="text-xs">{{ $post->excerpt }}</p>
                 </div>
                 @endforeach
-              </tbody>
-          </table>
+          </div>
+        {{ $posts->links() }}
       </div>
-    {{ $posts->links() }}
   </body>
 </html>
